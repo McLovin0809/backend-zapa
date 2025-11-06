c7489d076554a3c9e54ec549311ef74d5e991846
McLovin0809 <m.belmar@duocuc.cl>
2025-11-06 14:56:24 -0300

    Initial commit

---
+++ src/main/java/com/ecomerce/zapa/ZapaApplication.java
@@ -0,0 +1,13 @@
+package com.ecomerce.zapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomerce/zapa/ZapaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.zapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
