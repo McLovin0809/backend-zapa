17e143d63399ae9dedd093aad41983ffd9fa4a1e
Javier nc. <ja.niloc@duocuc.cl>
2025-11-13 16:50:14 -0300

    add de archivos config

---
+++ src/main/java/com/ecomerce/zapa/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.ecomerce.zapa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder contraSecret() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
