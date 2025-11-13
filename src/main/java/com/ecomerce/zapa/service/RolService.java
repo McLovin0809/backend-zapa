4d126693e26aa84b6ae4b61e395f49043dabdc48
POAPCA02LC0817 <SSDD@POAPCA02LC0817.duoc.acad>
2025-11-13 14:59:51 -0300

    roles

---
+++ src/main/java/com/ecomerce/zapa/service/RolService.java
@@ -0,0 +1,33 @@
+package com.ecomerce.zapa.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecomerce.zapa.model.Rol;
+import com.ecomerce.zapa.repository.RolRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+@SuppressWarnings("null")
+public class RolService {
+    
+    @Autowired
+    private RolRepository rolRepository;
+
+    public List<Rol> findAll(){
+        return rolRepository.findAll();
+    }
+
+    public Rol findById(Integer id) {
+        return rolRepository.findById(id).orElse(null);
+    }
+
+    
+    public Rol save(Rol rol) {
+        return rolRepository.save(rol);
+    } 
+}
