cd7fc51b601f4ec644be0c3c58c8b5739726d7b7
Javier nc. <ja.niloc@duocuc.cl>
2025-12-06 20:02:55 -0300

    update de imports

--- src/main/java/com/ecomerce/zapa/controller/DireccionController.java
+++ src/main/java/com/ecomerce/zapa/controller/DireccionController.java
@@ -1,7 +1,16 @@
 package com.ecomerce.zapa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ecomerce.zapa.model.Direccion;
 import com.ecomerce.zapa.service.DireccionService;
 import io.swagger.v3.oas.annotations.Operation;
--- src/main/java/com/ecomerce/zapa/controller/MetodoPagoController.java
+++ src/main/java/com/ecomerce/zapa/controller/MetodoPagoController.java
@@ -4,12 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ecomerce.zapa.model.MetodoPago;
 import com.ecomerce.zapa.service.MetodoPagoService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/metodos-pago")
