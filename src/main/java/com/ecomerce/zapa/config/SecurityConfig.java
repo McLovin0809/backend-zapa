565541f3762d7e840ce558473eeeaecadb5295a9
Javier nc. <ja.niloc@duocuc.cl>
2025-12-10 01:16:43 -0300

    201201

--- src/main/java/com/ecomerce/zapa/config/SecurityConfig.java
+++ src/main/java/com/ecomerce/zapa/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ecomerce.zapa.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,31 +10,39 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.ecomerce.zapa.security.JwtFilter;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-        .cors(cors -> {}) // FORMA CORRECTA PARA SPRING 6+
-        .csrf(csrf -> csrf.disable())
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers(
-                    "/api/usuarios/login",
-                    "/api/usuarios/registro",
-                    "/api/productos/**",
-                    "/swagger-ui/**",
-                    "/v3/api-docs/**"
-            ).permitAll()
-            .anyRequest().authenticated()
-        );
+            .cors(cors -> {})
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/api/usuarios/login",
+                        "/api/usuarios/registro",
+                        "/api/productos/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
-    return http.build();
+        return http.build();
     }
 
     @Bean
